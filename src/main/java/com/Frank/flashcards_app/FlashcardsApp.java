54fee322349d698a6544701df85d714cd3ae3ca8
frank <ochoa.franco132@gmail.com>
2024-08-24 22:27:07 -0300

    setting up the project

---
+++ src/main/java/com/Frank/flashcards_app/FlashcardsApp.java
@@ -0,0 +1,13 @@
+package com.Frank.flashcards_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardsApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcardsApp.class, args);
+	}
+
+}
