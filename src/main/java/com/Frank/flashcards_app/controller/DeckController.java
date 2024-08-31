6c56a01c2403b0d0d3bf1efa13d29ff6be7b6313
frank <ochoa.franco132@gmail.com>
2024-08-31 17:44:04 -0300

    adding CRUD functionality for deck entity

--- src/main/java/com/Frank/flashcards_app/controller/DeckController.java
+++ src/main/java/com/Frank/flashcards_app/controller/DeckController.java
@@ -1,7 +1,51 @@
 package com.Frank.flashcards_app.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.Frank.flashcards_app.dto.DeckRequestDTO;
+import com.Frank.flashcards_app.dto.DeckResponseDTO;
+import com.Frank.flashcards_app.service.IDeckService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/decks")
 public class DeckController {
+    @Autowired
+    IDeckService deckService;
+
+    // Create deck.
+    @PostMapping("/create")
+    public ResponseEntity<DeckResponseDTO> createDeck(@Valid @RequestBody DeckRequestDTO deckRequestDTO) {
+        DeckResponseDTO response = deckService.saveDeck(deckRequestDTO);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    // Read all decks.
+    @GetMapping()
+    public ResponseEntity<List<DeckResponseDTO>> getDecks() {
+        return new ResponseEntity<>(deckService.getDecks(), HttpStatus.OK);
+    }
+
+    // Read one deck.
+    @GetMapping("/{id}")
+    public ResponseEntity<DeckResponseDTO> getDeckById(@PathVariable Long id) {
+        return new ResponseEntity<>(deckService.getDeckById(id), HttpStatus.OK);
+    }
+
+    // update a deck by its ID.
+    @PutMapping("/edit/{id}")
+    public ResponseEntity<DeckResponseDTO> editDeck(@PathVariable Long id, @Valid @RequestBody DeckRequestDTO deckRequestDTO) {
+        return new ResponseEntity<>(deckService.editDeck(id, deckRequestDTO), HttpStatus.OK);
+    }
+
+    // Delete a deck by its ID.
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteDeck(@PathVariable Long id) {
+        deckService.deleteDeck(id);
+        return new ResponseEntity<>("Deck deleted successfully", HttpStatus.OK);
+    }
 }
