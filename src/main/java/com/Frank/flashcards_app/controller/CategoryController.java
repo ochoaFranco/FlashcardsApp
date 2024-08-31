73ec3ebe8ec7d2bc7a0d54db2ad9942676358f60
frank <ochoa.franco132@gmail.com>
2024-08-30 21:27:03 -0300

    working ok CRUD functionality

--- src/main/java/com/Frank/flashcards_app/controller/CategoryController.java
+++ src/main/java/com/Frank/flashcards_app/controller/CategoryController.java
@@ -6,10 +6,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
@@ -19,8 +18,31 @@ public class CategoryController {
 
     // Create category.
     @PostMapping("/create")
-    public ResponseEntity<?> createCategory(@Valid @RequestBody CategoryDTO categoryDTO) {
+    public ResponseEntity<CategoryDTO> createCategory(@Valid @RequestBody CategoryDTO categoryDTO) {
         CategoryDTO categoryDTOResponse= categoryService.saveCategory(categoryDTO);
         return new ResponseEntity<>(categoryDTOResponse, HttpStatus.CREATED);
+    }
+
+    // Read all categories.
+    @GetMapping()
+    public ResponseEntity<List<CategoryDTO>> getCategories() {
+        return new ResponseEntity<>(categoryService.getCategories(), HttpStatus.OK);
+    }
+
+    // Read one category.
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryDTO> getCategoryById(@PathVariable Long id) {
+        return new ResponseEntity<>(categoryService.getCategoryById(id), HttpStatus.OK);
+    }
+
+    // update a category by its ID.
+    @PutMapping("/edit/{id}")
+    public ResponseEntity<CategoryDTO> editCategory(@PathVariable Long id, @RequestBody @Valid CategoryDTO categoryDTO) {
+        return new ResponseEntity<>(categoryService.editCategory(id, categoryDTO ), HttpStatus.OK);
+    }
+    // Delete a category by its ID.
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteCategory(@PathVariable Long id) {
+        return new ResponseEntity<>("Category deleted successfully", HttpStatus.OK);
     }
 }
