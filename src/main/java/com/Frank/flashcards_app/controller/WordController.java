cfb18c3002e891b64e76f91b5c256e63b31fc852
frank <ochoa.franco132@gmail.com>
2024-08-31 15:54:34 -0300

    working on words CRUD

--- src/main/java/com/Frank/flashcards_app/controller/WordController.java
+++ src/main/java/com/Frank/flashcards_app/controller/WordController.java
@@ -7,10 +7,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/words")
@@ -23,5 +22,30 @@ public class WordController {
     public ResponseEntity<WordResponseDTO> createWord(@Valid @RequestBody WordRequestDTO wordRequestDTO) {
         WordResponseDTO wordResponseDTO = wordService.saveWord(wordRequestDTO);
         return new ResponseEntity<>(wordResponseDTO, HttpStatus.CREATED);
+    }
+
+    // Read all words
+    @GetMapping()
+    public ResponseEntity<List<WordResponseDTO>> getWords() {
+        return new ResponseEntity<>(wordService.getWords(), HttpStatus.OK);
+    }
+
+    // Read one category.
+    @GetMapping("/{id}")
+    public ResponseEntity<WordResponseDTO> getWordById(@PathVariable Long id) {
+        return new ResponseEntity<>(wordService.getWordById(id), HttpStatus.OK);
+    }
+
+    // update a word by its ID.
+    @PutMapping("/edit/{id}")
+    public ResponseEntity<WordResponseDTO> editWord(@PathVariable Long id, @Valid @RequestBody WordRequestDTO wordRequestDTO) {
+        return new ResponseEntity<>(wordService.editWord(id, wordRequestDTO), HttpStatus.OK);
+    }
+
+    // Delete a word by its ID.
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteWord(@PathVariable Long id) {
+        wordService.deleteWord(id);
+        return new ResponseEntity<>("Word deleted successfully", HttpStatus.OK);
     }
 }
